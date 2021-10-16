a219edf88911d6eaa635e6377a76bcaaa405d817
Saramelissa <saramelissa169@gmail.com>
2021-10-15 21:26:16 -0300

    Programas em JS feitos no Visual Code

---
+++ Sara Melissa/01.js
@@ -0,0 +1,9 @@
+//primeiro programa em java script
+//console.log('Olá, mundo');
+var nome = 'Sara';
+var sobrenome = 'Melissa';
+var nomecompleto;
+ nomecompleto = nome + sobrenome;
+ console.log(nome + ' ' + sobrenome);
+var idade = 19;
+ console.log(Meu nome é ' + ' ' + nome + ' ' + 'Tenho:' idade);
---
+++ Sara Melissa/03.js
@@ -0,0 +1,6 @@
+var num1 = 20;
+var num2 = 20;
+var num3 = 5;
+var num4 = 10;
+var valor = ((num1 + num2 + num4 - num4) / num3);
+console.log(valor);
